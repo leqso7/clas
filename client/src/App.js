d6a006a5599703e43432111cfb038c64080b7e92
leqso7 <115240888+leqso7@users.noreply.github.com>
2024-12-14 01:12:09 +0400

    Update project files with latest changes

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/clas">
       <div className="App">
         <Routes>
           <Route path="/" element={<RequestForm />} />
